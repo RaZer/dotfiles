03e8f690c59147bc131b7b9c75a18f85db919432
Serge Stupachenko <sergey.stupachenko@gmail.com>
2019-02-04 17:22:23 +0200

    Cleanup initiaal fonts intallation

--- bootstrap/20_homebrew_casks.sh
+++ bootstrap/20_homebrew_casks.sh
@@ -5,7 +5,6 @@ export HOMEBREW_CASK_OPTS="--appdir=/Applications --fontdir=/Library/Fonts"
 
 # TODO: Sort these casks
 brew cask install tunnelblick
-brew cask install station
 
 brew cask install appcleaner
 brew cask install dash
@@ -41,12 +40,10 @@ brew cask install caskroom/fonts/font-inconsolata-lgc
 brew cask install caskroom/fonts/font-inconsolata-for-powerline
 brew cask install caskroom/fonts/font-inconsolata-g-for-powerline
 brew cask install caskroom/fonts/font-liberation-mono-for-powerline
-brew cask install caskroom/fonts/font-meslo-lg-for-powerline
-brew cask install caskroom/fonts/font-sauce-code-powerline
 brew cask install caskroom/fonts/font-source-code-pro
 brew cask install caskroom/fonts/font-source-code-pro-for-powerline
-brew cask install caskroom/fonts/font-fira-mono-for-powerline
-
+brew cask install caskroom/fonts/font-fira-code
+brew cask install caskroom/fonts/font-menlo-for-powerline
 
 
 
